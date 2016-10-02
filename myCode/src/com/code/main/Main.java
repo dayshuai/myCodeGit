becad9d433a3b1e39cd68eea954a0aef7034edc7
dayshuai <hs159@qq.com>
2016-10-02 10:16:21 +0800

    wwwwwwwwww

--- myCode/src/com/code/main/Main.java
+++ myCode/src/com/code/main/Main.java
@@ -27,7 +27,6 @@ public class Main {
 	 }
 	
 	public static void main(String[] args) {
-		//mygithub testfss
 		File projectDir = new File("");
 		String templatePath = projectDir.getAbsolutePath() + SystemConstant.JavaFileConstants.TEMPLATE_PATH;
 		String [] filesArr = getFileName(templatePath);
