f7ebbe2d089913b599c6f1c6577af42d6b5886b4
dayshuai <hs159@qq.com>
2016-10-10 23:23:07 +0800

    dayshuai

--- myCode/src/com/code/main/Main.java
+++ myCode/src/com/code/main/Main.java
@@ -1,14 +1,17 @@
 package com.code.main;
 
-import java.io.File;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.code.base.SystemConstant;
+import com.code.bean.Column;
+import com.code.bean.Table;
+import com.code.bean.TemplateConfig;
 import com.code.produce.FreemarkerUtil;
-import com.code.produce.ProduceUtil;
+import com.code.produce.ProduceRun;
 
 public class Main {
 	private static final Logger logger = LoggerFactory.getLogger(FreemarkerUtil.class);
@@ -21,10 +24,49 @@ public class Main {
 		 * ProduceUtil.getFileName(templatePath);
 		 * ProduceUtil.getJavaFile(filesArr);
 		 */
-		String str = "aaBBcC";
-		str = camelToUnderline(str);
-		System.out.println(str);
+	/*	Database database = new Database();
+		System.out.println(database.getClass().getSimpleName());
 
+		System.out.println(database.getClass().getCanonicalName());
+
+		String[] fieldNames = getFiledName(database);
+		for (String string : fieldNames) {
+			System.out.println(string);
+		}*/
+		//beanToTable(new TemplateConfig());
+		/*String[] fieldNames = getFiledName(new TemplateConfig());
+		for (String fieldName : fieldNames) {
+			Column c = new Column();
+			//实体属性名
+			//c.setColumnName(fieldName);
+			System.out.println(fieldName + " " + camelToUnderline(fieldName));
+			//数据库表字段名
+			//c.setMappingName(camelToUnderline(fieldName));
+		}*/
+	}
+
+	public static Table beanToTable(Object o) {
+		Table table = new Table();
+		String tableMappingName = camelToUnderline(ProduceRun.firstToLower(o.getClass().getSimpleName()));
+		System.out.println(tableMappingName);
+		// 数据库表名
+		table.setMappingName(tableMappingName);
+		table.setTableName(o.getClass().getSimpleName());
+		List <Column> columnList = new ArrayList <Column> ();
+		String[] fieldNames = getFiledName(o);
+		for (String fieldName : fieldNames) {
+			if (!fieldName.equals("serialVersionUID")) {
+				Column c = new Column();
+				//实体属性名
+				c.setColumnName(fieldName);
+				//数据库表字段名
+				c.setMappingName(camelToUnderline(fieldName));
+			}
+			
+		}
+		table.setColumnList(columnList);
+
+		return table;
 	}
 
 	public static String[] getFiledName(Object o) {
@@ -35,27 +77,28 @@ public class Main {
 		}
 		return fieldNames;
 	}
-	
+
 	/**
 	 * 驼峰转化
 	 */
-	public static final char UNDERLINE='_';  
-	public static String camelToUnderline(String param){  
-	    if (param==null||"".equals(param.trim())){  
-	    return "";  
-	    }  
-	    int len=param.length();  
-	    StringBuilder sb=new StringBuilder(len);  
-	    for (int i = 0; i < len; i++) {  
-	        char c=param.charAt(i);  
-	        if (Character.isUpperCase(c)){  
-	            sb.append(UNDERLINE);  
-	            sb.append(Character.toLowerCase(c));  
-	        }else{  
-	            sb.append(c);  
-	        }  
-	    }  
-	    return sb.toString();  
-	} 
+	public static final char UNDERLINE = '_';
+
+	public static String camelToUnderline(String param) {
+		if (param == null || "".equals(param.trim())) {
+			return "";
+		}
+		int len = param.length();
+		StringBuilder sb = new StringBuilder(len);
+		for (int i = 0; i < len; i++) {
+			char c = param.charAt(i);
+			if (Character.isUpperCase(c)) {
+				sb.append(UNDERLINE);
+				sb.append(Character.toLowerCase(c));
+			} else {
+				sb.append(c);
+			}
+		}
+		return sb.toString();
+	}
 
 }
--- myCode/src/com/code/produce/ProduceRun.java
+++ myCode/src/com/code/produce/ProduceRun.java
@@ -1,7 +1,10 @@
 package com.code.produce;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.swing.filechooser.FileSystemView;
@@ -22,16 +25,20 @@ public class ProduceRun extends Thread {
 	private String produceType;
 	private String fileName;
 	private String beanPackage;
+	private Object object;
+	private String javaPrimary;
 
 	private Map<String, Object> contentMap = new HashMap<String, Object>();
 
-	public ProduceRun(String templatePath, String writePath, String projectName, String produType, String fileName) {
+	public ProduceRun(String templatePath, String writePath, String projectName, String produType, String fileName, Object object, String javaPrimary) {
 
 		this.templatePath = templatePath;
 		this.writePath = writePath;
 		this.projectName = projectName;
 		this.produceType = produType;
 		this.fileName = fileName;
+		this.object = object;
+		this.javaPrimary = javaPrimary;
 		initPrepare();
 	}
 
@@ -57,7 +64,7 @@ public class ProduceRun extends Thread {
 	}
 
 	public void run() {
-
+		produceDAOXml("DaoImpl.ftl", this.object, this.javaPrimary);
 		try {
 			String cmd = "cmd /c start " + this.writePath.replace(this.projectName, "");
 			Runtime.getRuntime().exec(cmd);
@@ -82,7 +89,8 @@ public class ProduceRun extends Thread {
 		return false;
 	}
 
-	public static void produceDAOXml(String templateName, Table t) {
+	public static void produceDAOXml(String templateName, Object object, String javaPrimary) {
+		Table t = beanToTable(object, javaPrimary);
 		String fileName = "";
 		String writePath = "";
 		File desktopDir = FileSystemView.getFileSystemView().getHomeDirectory();
@@ -124,5 +132,65 @@ public class ProduceRun extends Thread {
 			return "";
 		}
 		return String.valueOf(str.charAt(0)).toLowerCase() + str.substring(1, str.length());
+	}
+	
+	public static Table beanToTable(Object o, String javaPrimary) {
+		Table table = new Table();
+		String tableMappingName = camelToUnderline(firstToLower(o.getClass().getSimpleName()));
+		System.out.println(tableMappingName);
+		// 数据库表名
+		table.setMappingName(tableMappingName);
+		table.setTableName(o.getClass().getSimpleName());
+		List <Column> columnList = new ArrayList <Column> ();
+		String[] fieldNames = getFiledName(o);
+		for (String fieldName : fieldNames) {
+			if (!fieldName.equals("serialVersionUID")) {
+				
+				Column c = new Column();
+				if (fieldName.equals(javaPrimary)) {
+					c.setIsPrimary("YES");
+				}
+				//实体属性名
+				c.setColumnName(fieldName);
+				//数据库表字段名
+				c.setMappingName(camelToUnderline(fieldName));
+			}
+			
+		}
+		table.setColumnList(columnList);
+
+		return table;
+	}
+
+	public static String[] getFiledName(Object o) {
+		Field[] fields = o.getClass().getDeclaredFields();
+		String[] fieldNames = new String[fields.length];
+		for (int i = 0; i < fields.length; i++) {
+			fieldNames[i] = fields[i].getName();
+		}
+		return fieldNames;
+	}
+
+	/**
+	 * 驼峰转化
+	 */
+	public static final char UNDERLINE = '_';
+
+	public static String camelToUnderline(String param) {
+		if (param == null || "".equals(param.trim())) {
+			return "";
+		}
+		int len = param.length();
+		StringBuilder sb = new StringBuilder(len);
+		for (int i = 0; i < len; i++) {
+			char c = param.charAt(i);
+			if (Character.isUpperCase(c)) {
+				sb.append(UNDERLINE);
+				sb.append(Character.toLowerCase(c));
+			} else {
+				sb.append(c);
+			}
+		}
+		return sb.toString();
 	}
 }
