71937f168145c40749ce3ce7489c280780d436f8
dayshuai <hs159@qq.com>
2016-10-10 23:24:23 +0800

    dayshuai

--- myCode/src/com/code/main/Main.java
+++ myCode/src/com/code/main/Main.java
@@ -43,62 +43,10 @@ public class Main {
 			//数据库表字段名
 			//c.setMappingName(camelToUnderline(fieldName));
 		}*/
+		
+		
 	}
 
-	public static Table beanToTable(Object o) {
-		Table table = new Table();
-		String tableMappingName = camelToUnderline(ProduceRun.firstToLower(o.getClass().getSimpleName()));
-		System.out.println(tableMappingName);
-		// 数据库表名
-		table.setMappingName(tableMappingName);
-		table.setTableName(o.getClass().getSimpleName());
-		List <Column> columnList = new ArrayList <Column> ();
-		String[] fieldNames = getFiledName(o);
-		for (String fieldName : fieldNames) {
-			if (!fieldName.equals("serialVersionUID")) {
-				Column c = new Column();
-				//实体属性名
-				c.setColumnName(fieldName);
-				//数据库表字段名
-				c.setMappingName(camelToUnderline(fieldName));
-			}
-			
-		}
-		table.setColumnList(columnList);
-
-		return table;
-	}
-
-	public static String[] getFiledName(Object o) {
-		Field[] fields = o.getClass().getDeclaredFields();
-		String[] fieldNames = new String[fields.length];
-		for (int i = 0; i < fields.length; i++) {
-			fieldNames[i] = fields[i].getName();
-		}
-		return fieldNames;
-	}
-
-	/**
-	 * 驼峰转化
-	 */
-	public static final char UNDERLINE = '_';
-
-	public static String camelToUnderline(String param) {
-		if (param == null || "".equals(param.trim())) {
-			return "";
-		}
-		int len = param.length();
-		StringBuilder sb = new StringBuilder(len);
-		for (int i = 0; i < len; i++) {
-			char c = param.charAt(i);
-			if (Character.isUpperCase(c)) {
-				sb.append(UNDERLINE);
-				sb.append(Character.toLowerCase(c));
-			} else {
-				sb.append(c);
-			}
-		}
-		return sb.toString();
-	}
+	
 
 }
